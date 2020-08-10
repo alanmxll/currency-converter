1cefbce582613c78da1e13df9b3413387ec79fad
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-08-10 15:26:01 -0300

    implemented ViewHolder pattern

--- app/src/main/java/com/alanmxll/currencyconverter/MainActivity.java
+++ app/src/main/java/com/alanmxll/currencyconverter/MainActivity.java
@@ -3,12 +3,31 @@ package com.alanmxll.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDolar;
+        TextView textEuro;
+        Button buttonCalculate;
     }
 }
